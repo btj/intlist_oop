5ef5411eac6628bfee4c16deb8729e0835d6d7fc
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-02-28 12:53:53 +0100

    Optimized array-based representation

--- intlist_oop/src/intlist_oop/IntList.java
+++ intlist_oop/src/intlist_oop/IntList.java
@@ -11,10 +11,13 @@ public class IntList {
 	/**
 	 * @invar {@code elements} is not null
 	 *     | elements != null
+	 * @invar {@code size} is between 0 and the length of {@code elements}
+	 *     | 0 <= size && size <= elements.length
 	 */
-	private int[] elements = new int[0];
+	private int size;
+	private int[] elements = new int[10];
 	
-	public int getSize() { return elements.length; }
+	public int getSize() { return size; }
 
 	/**
 	 * @post The result equals the element at the given index of this object's list of elements
@@ -25,7 +28,7 @@ public class IntList {
 	public int get(int index) { return elements[index]; }
 	
 	public int[] getElements() {
-		return Arrays.copyOf(elements, elements.length);
+		return Arrays.copyOf(elements, size);
 	}
 	
 	/**
@@ -46,11 +49,21 @@ public class IntList {
 	 * @param value
 	 */
 	public void add(int value) {
-		int[] newElements = new int[elements.length + 1];
-		for (int i = 0; i < elements.length; i++)
-			newElements[i] = elements[i];
-		newElements[elements.length] = value;
-		elements = newElements;
+		if (size == elements.length) {
+			int[] newElements = new int[elements.length * 2];
+			for (int i = 0; i < elements.length; i++)
+				newElements[i] = elements[i];
+			newElements[elements.length] = value;
+			elements = newElements;
+			size++;
+		} else {
+			elements[size] = value;
+			size = size + 1;
+			// size += 1;
+			// size++;
+			
+			// elements[size++] = value;
+		}
 	}
 
 }
--- intlist_oop/src/intlist_oop/IntListTest.java
+++ intlist_oop/src/intlist_oop/IntListTest.java
@@ -13,11 +13,22 @@ class IntListTest {
 		IntList myList = new IntList();
 		myList.add(7);
 		myList.add(42);
+		
 		assert myList.getSize() == 2;
 		assert myList.get(0) == 7;
 		assert myList.get(1) == 42;
 		assert Arrays.equals(myList.getElements(), new int[] {7, 42});
 		//assert myList.getElements() == new int[] {7, 42}; // NOT CORRECT
+	}
+	
+	@Test
+	void bigTest() {
+		IntList myList = new IntList();
+		for (int i = 0; i < 1000; i++)
+			myList.add(13 * i);
+		assert myList.getSize() == 1000;
+		for (int i = 0; i < 1000; i++)
+			assert myList.get(i) == 13 * i;
 	}
 
 }
