a69d3f4d5216877faa8c1fadf3215fc3ace27d8c
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-03-06 11:40:19 +0100

    Linked list-based implementation

--- intlist_oop/src/intlist_oop/IntList.java
+++ intlist_oop/src/intlist_oop/IntList.java
@@ -2,33 +2,58 @@ package intlist_oop;
 
 import java.util.Arrays;
 
+class Node {
+	int value;
+	Node next;
+}
+
 /**
  * @invar This object's size equals the number of elements
  *     | getSize() == getElements().length
  */
 public class IntList {
 	
-	/**
-	 * @invar {@code elements} is not null
-	 *     | elements != null
-	 * @invar {@code size} is between 0 and the length of {@code elements}
-	 *     | 0 <= size && size <= elements.length
-	 */
-	private int size;
-	private int[] elements = new int[10];
+	private Node firstNode;
 	
-	public int getSize() { return size; }
+	public int getSize() {
+		int size = 0;
+		Node currentNode = firstNode;
+		while (currentNode != null) {
+			size++;
+			currentNode = currentNode.next;
+		}
+		return size;
+	}
 
+//	 * @pre The index is within bounds
+//	 *     | 0 <= index && index < getSize()
 	/**
+	 * Returns the value at the given index (zero-based)
+	 * in this list of integers.
+	 * 
+	 * @throws IllegalArgumentException if the index is out of range
+	 *     | !(0 <= index && index < getSize())
 	 * @post The result equals the element at the given index of this object's list of elements
 	 *     | result == getElements()[index]
-	 * @throws The given index is between 0 (inclusive) and this object's size (exclusive) 
-	 *     | 0 <= index && index < getSize()
 	 */
-	public int get(int index) { return elements[index]; }
+	public int get(int index) {
+		if (!(0 <= index && index < getSize()))
+			throw new IllegalArgumentException("index out of range");
+		
+		Node currentNode = firstNode;
+		for (int i = 0; i < index; i++)
+			currentNode = currentNode.next;
+		return currentNode.value;
+	}
 	
 	public int[] getElements() {
-		return Arrays.copyOf(elements, size);
+		int[] elements = new int[getSize()];
+		Node currentNode = firstNode;
+		for (int i = 0; i < elements.length; i++) {
+			elements[i] = currentNode.value;
+			currentNode = currentNode.next;
+		}
+		return elements;
 	}
 	
 	/**
@@ -38,31 +63,25 @@ public class IntList {
 	public IntList() {}
 	
 	/**
+	 * Adds the given value to the end of this list of integers.
+	 * 
 	 * @post This object's size equals its old size plus one.
 	 *     | getSize() == old(getSize()) + 1
 	 * @post This object's existing elements remain unchanged.
 	 *     | Arrays.equals(getElements(), 0, old(getSize()), old(getElements()), 0, old(getSize()))
 	 * @post This object's last elements equals the given value.
 	 *     | getElements()[old(getSize())] == value
-	 *     | // get(old(getSize()) == value
-	 *     | // get(getSize() - 1) == value
-	 * @param value
 	 */
 	public void add(int value) {
-		if (size == elements.length) {
-			int[] newElements = new int[elements.length * 2];
-			for (int i = 0; i < elements.length; i++)
-				newElements[i] = elements[i];
-			newElements[elements.length] = value;
-			elements = newElements;
-			size++;
+		if (firstNode != null) {
+			Node currentNode = firstNode;
+			while (currentNode.next != null)
+				currentNode = currentNode.next;
+			currentNode.next = new Node();
+			currentNode.next.value = value;
 		} else {
-			elements[size] = value;
-			size = size + 1;
-			// size += 1;
-			// size++;
-			
-			// elements[size++] = value;
+			firstNode = new Node();
+			firstNode.value = value;
 		}
 	}
 
